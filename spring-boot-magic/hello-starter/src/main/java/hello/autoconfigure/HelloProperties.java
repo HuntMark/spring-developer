f13475f6cf882375163d1011aef0a41bba08899f
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-12-11 15:32:15 +0300

    Configurable auto-configuration

---
+++ spring-boot-magic/hello-starter/src/main/java/hello/autoconfigure/HelloProperties.java
@@ -0,0 +1,29 @@
+package hello.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("hello")
+public class HelloProperties {
+
+    /**
+     * Prefix of the welcome message.
+     */
+    private String prefix = "Hello";
+    private String suffix = "!";
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+}
