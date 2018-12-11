95ec7519888e35571880c061262032be1c0a78ee
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-12-11 15:00:32 +0300

    Improve auto-configuration with conditions

--- spring-boot-magic/hello-starter/src/test/java/hello/autoconfigure/HelloAutoConfigurationTest.java
+++ spring-boot-magic/hello-starter/src/test/java/hello/autoconfigure/HelloAutoConfigurationTest.java
@@ -1,12 +1,16 @@
 package hello.autoconfigure;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.ConsoleHelloService;
 import hello.HelloService;
-import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.context.annotation.Bean;
 
 public class HelloAutoConfigurationTest {
 
@@ -19,9 +23,28 @@ public class HelloAutoConfigurationTest {
     @Test
     public void defaultServiceIsAutoConfigured() {
         this.contextRunner.run(context -> {
-            Assertions.assertThat(context).hasSingleBean(HelloService.class);
+            assertThat(context).hasSingleBean(HelloService.class);
             context.getBean(HelloService.class).sayHello("World");
-            Assertions.assertThat(this.output.toString()).contains("Hello World!");
+            assertThat(this.output.toString()).contains("Hello World!");
         });
+    }
+
+    @Test
+    public void defaultServiceBacksOff() {
+        this.contextRunner
+                .withUserConfiguration(UserConfiguration.class)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(HelloService.class);
+                    context.getBean(HelloService.class).sayHello("Works");
+                    assertThat(this.output.toString()).contains("Mine Works**");
+                });
+    }
+
+    @TestConfiguration
+    static class UserConfiguration {
+        @Bean
+        public HelloService myHelloService() {
+            return new ConsoleHelloService("Mine", "**");
+        }
     }
 }
